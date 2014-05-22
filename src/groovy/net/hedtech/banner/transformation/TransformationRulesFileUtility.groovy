abce2a97727939fac5c176d9e25183d7ccc85f54
Harry van Thor <harry.van.thor@ellucian.com>
2014-05-22 17:31:07 +0200

    EXTZ-485: Implement support for 2 xml documents/env. vars

--- src/groovy/net/hedtech/banner/transformation/TransformationRulesFileUtility.groovy
+++ src/groovy/net/hedtech/banner/transformation/TransformationRulesFileUtility.groovy
@@ -1,38 +1,98 @@
 package net.hedtech.banner.transformation
 
+import grails.converters.deep.JSON
+import groovy.time.TimeCategory
+
 class TransformationRulesFileUtility {
 
-    private static final String fileLocation = System.getenv('BANNER_TRANSFORMATION')
-
-    private static Node transformationRules = null
+    private static final def envName = 'BANNER_TRANSFORMATION'
+    private static Map transformationRules = null
 
     static boolean transformationRulesExist() {
-        if (!fileLocation) {
-            return false
+        if (transformationRules == null) {
+            loadRules()
         }
-
-        File transformationRulesFile = new File(fileLocation)
-        transformationRules = new XmlParser().parse(transformationRulesFile)
-
-        return transformationRules ? transformationRules instanceof Node : false
+        return transformationRules.size()>0
     }
-
 
     static Map rulesForClass(String className) {
         if (transformationRules == null) {
-            transformationRulesExist()
+            loadRules()
         }
+        return transformationRules.get(className)
+    }
 
-        Map rules = [:]
-        if (transformationRules instanceof Node) {
-            for (Node ruleNode : transformationRules.value()) {
-                if (ruleNode.get("@id") == className) {
-                    def text = ruleNode.value()[0]
-                    rules = new GroovyShell().evaluate(text)
-                    break;
+    private static void loadRules(){
+        def t0 = new Date()
+        def xmlParser = new XmlParser()
+        def loadCount = 0
+        // first time set rules to a not null value, this way system only attempts to load once
+        transformationRules = [:]
+        //load up to 2 xml files, can be extended to any number without significant change
+        for (int i=1; i<3; i++)  {
+            def fileLocation = System.getenv("$envName$i")
+            if (fileLocation){
+                File xmlFile = new File(fileLocation)
+                if (xmlFile.exists()){
+                    def xmlRules = xmlParser.parse(xmlFile)
+                    addRules(convertNode(xmlRules))
+                    println "Loaded rules from $fileLocation"
+                    loadCount++
                 }
             }
         }
-        return rules
+        def t1 = new Date()
+        if (loadCount) {
+            println "Loaded metadata in ${TimeCategory.minus(t1, t0)}"
+        }
+        //printMap()
+    }
+
+    private static void addRules(Map rules) {
+        if (rules?.size()) {
+            ArrayList rulesArray = []
+            if (transformationRules) {
+                rulesArray.add(transformationRules)
+            }
+            rulesArray.add(rules)
+            transformationRules = mergeMaps(rulesArray)
+        }
+    }
+
+    private static Map convertNode(Node xmlNode)  {
+        Map  groovyRules =[:]
+        if (xmlNode instanceof Node) {
+            def shell = new GroovyShell()
+            for (Node ruleNode : xmlNode.value()) {
+                def domainName = ruleNode.get("@id")
+                if (domainName) {
+                    def text = ruleNode.value()[0]
+                    def domainRules = shell.evaluate(text)
+                    groovyRules << [(domainName): domainRules]
+                }
+            }
+        }
+        return groovyRules
+    }
+
+    static Map mergeMaps(ArrayList<Map> maps) {
+        if (!maps?.size())
+            throw new Exception("List of metadata must have at least one entry")
+        // Use merge method in ConfigObject to merge the maps
+        def result = new ConfigObject()
+        result.putAll(maps[0])
+        for (int i = 1; i < maps.size(); i++) {
+            def added = new ConfigObject()
+            added.putAll(maps[i])
+            result.merge(added)
+        }
+        return result
+    }
+
+    static void printMap() {
+        transformationRules.each {
+            println "Domain: $it.key \n $it.value"
+        }
+
     }
 }
---
+++ test/unit/net/hedtech/banner/transformation/TransformationRulesFileUtilityTests.groovy
@@ -0,0 +1,77 @@
+package net.hedtech.banner.transformation
+
+import grails.test.mixin.*
+import grails.test.mixin.support.*
+/*
+To run this unit test, 2 environment variables are required.
+BANNER_TRANSFORMATION1=test/ast-definition/test-base.xml
+BANNER_TRANSFORMATION2=test/ast-definition/test-additional.xml
+Since it is impossible or very difficult to set environment variables within the test, they have to be set
+in the IDE or environment.
+ */
+@TestMixin(GrailsUnitTestMixin)
+class TransformationRulesFileUtilityTests {
+    void setUp() {
+        // Setup logic here
+    }
+
+    void tearDown() {
+        // Tear down logic here
+    }
+
+    void testMergeMetaData() {
+        def base = [
+                fields: [
+                        a: [
+                                type                 : "java.lang.String",
+                                persistenceProperties: [name: "db_a"]
+                        ],
+                        b: [
+                                type                 : "java.lang.String",
+                                persistenceProperties: [name: "db_b"]
+                        ]
+                ]
+        ]
+        def additional = [
+                fields : [
+                        b: [
+                                type                 : "java.lang.String",
+                                persistenceProperties: [name: "db_bprime"]  // can we change an attribute?
+                        ],
+                        //can we add a field?
+                        c: [
+                                type                 : "java.lang.String",
+                                persistenceProperties: [name: "db_c"]
+                        ]
+                ],
+                methods: [
+                        [
+                                name  : "methodA",
+                                source: "methodASource"
+                        ]
+
+                ]
+        ]
+
+        def result = TransformationRulesFileUtility.mergeMaps( [base, additional] )
+        assertNotNull(result.fields.a)
+        assertEquals("Failure changing field b","db_bprime",result.fields.b.persistenceProperties.name)
+        assertEquals("Failure adding field c","db_c",result.fields.c.persistenceProperties.name)
+        assertNotNull("Failed to add methods", result.methods)
+
+    }
+
+    //This test loads 2 xml files and checks if merged correctly
+    void testRulesForClass() {
+        def e1=TransformationRulesFileUtility.rulesForClass("ast.domain.ext.Employee")
+        def e2=TransformationRulesFileUtility.rulesForClass("Emp")
+        def a=TransformationRulesFileUtility.rulesForClass("AddedClass")
+        def d=TransformationRulesFileUtility.rulesForClass("Dept")
+        assertNotNull("Employee is not found",e1)
+        assertNotNull("Emp is not found",e2)
+        assertNotNull("AddedClass is not found",a)
+        assertNotNull("Dept is not found",d)
+        assertEquals("Unexpected persistenceProperties for surnamePrefix", "surname_prefix_new",e2.fields.surnamePrefix.persistenceProperties.name)
+        assertNotNull("Employee misses surnamePrefix field",e1.fields.surnamePrefix)
+    }
+}
