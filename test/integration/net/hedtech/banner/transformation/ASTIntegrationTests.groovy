39b3774a7173f9617791290ea88d7231b84438e0
Steve_Lambert <steve.lambert@ellucian.com>
2014-06-09 16:34:13 +0100

    Added integration tests

---
+++ test/integration/net/hedtech/banner/transformation/ASTIntegrationTests.groovy
@@ -0,0 +1,22 @@
+package net.hedtech.banner.transformation
+
+import static org.junit.Assert.*
+import org.junit.*
+
+class ASTIntegrationTests {
+
+    @Before
+    void setUp() {
+        // Setup logic here
+    }
+
+    @After
+    void tearDown() {
+        // Tear down logic here
+    }
+
+    @Test
+    void testSomething() {
+        fail "Implement me"
+    }
+}
